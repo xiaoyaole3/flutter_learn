55d34637c97775cf0230c445c0959dc43195a67c
wander <xiaoyaole3@163.com>
2022-06-05 15:08:37 +0800

    flutter expanded

---
+++ lib/expanded.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/res/listData.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// 自定义组件
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Person app bar"),
+        ),
+        body: HomeContent(),
+      ),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow
+      ),
+    );
+  }
+
+}
+
+class IconContainer extends StatelessWidget{
+  double? size = 32.0;
+  Color? color = Colors.red;
+  IconData icon;
+
+  IconContainer(this.icon, {this.color, this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      color: color,
+      child: Center(
+        child: Icon(
+          icon,
+          size: size,
+          color: Colors.white
+        ),
+      ),
+    );
+  }
+}
+
+
+class HomeContent extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded( // 自适应
+          flex: 1,
+          child: IconContainer(Icons.search, color: Colors.blue,)
+        ),
+        Expanded(
+          flex: 2,
+          child: IconContainer(Icons.home, color: Colors.orange,)
+        ),
+        Expanded(
+            flex: 1,
+            child: IconContainer(Icons.select_all, color: Colors.red,)
+        ),
+      ],
+    );
+    
+  }
+}
