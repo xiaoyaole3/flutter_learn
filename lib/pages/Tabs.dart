34e2a6f9db09bcd0fb85d1cbb26193c1f31165c4
wander <xiaoyaole3@163.com>
2022-06-06 00:26:27 +0800

    navigationBar组件中实现切换页签相关功能

---
+++ lib/pages/Tabs.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'tabs/Home.dart';
+import 'tabs/Category.dart';
+import 'tabs/Settings.dart';
+
+class Tabs extends StatefulWidget {
+  const Tabs({Key? key}) : super(key: key);
+
+  @override
+  State<Tabs> createState() => _TabsState();
+}
+
+class _TabsState extends State<Tabs> {
+  int _currentIndex = 0;
+
+  List _pageList = [
+    HomePage(),
+    CategoryPage(),
+    SettingsPage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Person app bar"),
+      ),
+      body: _pageList[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        // iconSize: 25,
+        type: BottomNavigationBarType.fixed, // 配置底部tabs可以有多个按钮，默认只能有三个
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "首页"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "设置"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              label: "分类"
+          ),
+
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/tabs.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/pages/Tabs.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Tabs(),
+      theme: ThemeData(
+          primarySwatch: Colors.yellow
+      ),
+    );
+  }
+
+}
+
