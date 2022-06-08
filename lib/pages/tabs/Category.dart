d21635854257ec4cacef69b7ddffc42dbe65b707
wander <xiaoyaole3@163.com>
2022-06-08 11:35:49 +0800

    Scaffold嵌套Scaffold可以看到两个appBar

--- lib/pages/tabs/Category.dart
+++ lib/pages/tabs/Category.dart
@@ -11,29 +11,48 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ElevatedButton(
-          child: Text("跳转到表单页面并传值"),
-          onPressed: () {
-            // 基础路由实现表单跳转传值
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context)=>FormPage(title: "这个是传入的参数")
-              )
-            );
-          },
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("嵌套的appbar"),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: "热销",),
+              Tab(text: "推荐",),
+            ],
+          ),
         ),
-        SizedBox(height: 10,),
-        ElevatedButton(
-          child: Text("跳转到表单页面并传值-命名路由"),
-          onPressed: () {
-            Navigator.pushNamed(context, '/form');
-          },
+        body: TabBarView(
+          children: [
+            Text("这个是第一个页面"),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  child: Text("跳转到表单页面并传值"),
+                  onPressed: () {
+                    // 基础路由实现表单跳转传值
+                    Navigator.of(context).push(
+                        MaterialPageRoute(
+                            builder: (context)=>FormPage(title: "这个是传入的参数")
+                        )
+                    );
+                  },
+                ),
+                SizedBox(height: 10,),
+                ElevatedButton(
+                  child: Text("跳转到表单页面并传值-命名路由"),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/form');
+                  },
+                ),
+              ],
+            )
+          ],
         ),
-      ],
+      ),
     );
   }
 }
