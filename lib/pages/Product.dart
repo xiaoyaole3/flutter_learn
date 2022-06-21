026818aba70a30f1eea6ee73d156ddcacae01554
wander <xiaoyaole3@163.com>
2022-06-21 15:20:34 +0800

    对于用状态组件，使用widget可以直接访问到父节点的参数信息。这是正确的对有状态组件的传递参数方式

--- lib/pages/Product.dart
+++ lib/pages/Product.dart
@@ -4,18 +4,14 @@ class ProductPage extends StatefulWidget {
 
   var arguments;
 
-
   ProductPage({this.arguments});
 
   @override
-  State<ProductPage> createState() => _ProductPageState(arguments: arguments);
+  State<ProductPage> createState() => _ProductPageState();
 }
 
 class _ProductPageState extends State<ProductPage> {
-  var arguments;
 
-
-  _ProductPageState({this.arguments});
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +21,7 @@ class _ProductPageState extends State<ProductPage> {
       ),
       body: Column(
         children: [
-          Text("这里获取对应的商品信息参数${arguments ?? "null"}"),
+          Text("这里获取对应的商品信息参数${widget.arguments ?? "null"}"),
           SizedBox(height: 10,),
           ElevatedButton(
             child: Text("跳转到商品详情页面"),
